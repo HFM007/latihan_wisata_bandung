29a2042072b4a2a3960b0aac9fc22e0792af70d8
HFM007 <habib.fm20@student.unibi.ac.id>
2024-10-29 21:44:00 +0700

    Menambahkan deskripsi dan icon

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,47 @@ class DetailScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 30.0,
-                  fontWeight: FontWeight.bold,),
+                  fontWeight: FontWeight.bold,
                 ),
+              ),
             ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20.0),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 9.0),
+                      Text("Open Everyday"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.access_time),
+                      SizedBox(height: 9.0),
+                      Text("09:00 - 20:00"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.monetization_on),
+                      SizedBox(height: 9.0),
+                      Text("Rp 25.000"),
+                    ],
+                  )
+                ],
+              )
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: const Text(
+                "Berada di jalur utama Bandung-Lembang, Farmhouse Lembang adalah tempat wisata yang sangat populer. Tempat ini memiliki desain arsitektur ala Eropa yang sangat kental. Pengunjung bisa berfoto-foto di spot-spot menarik yang ada di sini.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            )
           ],
         ),
       ),
