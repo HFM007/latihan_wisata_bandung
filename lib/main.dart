14e4149c20ad9c005d06f9108dcf838bb87da43d
HFM007 <habib.fm20@student.unibi.ac.id>
2024-10-29 21:18:05 +0700

    menambahkan kolum dan widget home

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,21 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      title: "Wisata Bandung",
+      // theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
       ),
     );
   }
